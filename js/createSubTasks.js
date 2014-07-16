fa273700739a91bc0d1cbf064463a08f0ec5fab4
Daniel McQuiston <daniel.pro@dehugo.net>
2014-07-16 15:21:53 -0600

    Cleaned up and organized JavaScript code.

--- js/createSubTasks.js
+++ js/createSubTasks.js
@@ -120,16 +120,91 @@
 	// BEGIN: Public methods
 	ui.init = function(){
 		$(SELECTOR.FORM.PARENT_ISSUE).on("click keypress","button,"+SELECTOR.INPUT.ISSUE_KEY,ui.issueSearchEvent);
-		$("nav").on("click",SELECTOR.BUTTON.CREATE_TASKS,createSubTask);
-		$("nav").on("click",SELECTOR.BUTTON.CLEAR_SELECTED,clearChecked);
-		$("nav").on("click",SELECTOR.BUTTON.SELECT_REQUIRED,checkreq);
-		$("nav").on("click",SELECTOR.BUTTON.SELECT_ALL,checkall);
-		$("nav").on("submit",SELECTOR.FORM.AUTHENTICATE,ui.loginEvent);
-		$("nav").on("click",SELECTOR.BUTTON.LOGOUT,ui.logoutEvent);
+		$("nav").on( "click",  SELECTOR.BUTTON.CREATE_TASKS,    ui.issueCreateSubTasks);
+		$("nav").on( "click",  SELECTOR.BUTTON.CLEAR_SELECTED,  ui.issuesUnmarkAll);
+		$("nav").on( "click",  SELECTOR.BUTTON.SELECT_REQUIRED, ui.issuesMarkRequired);
+		$("nav").on( "click",  SELECTOR.BUTTON.SELECT_ALL,      ui.issuesMarkAll);
+		$("nav").on( "submit", SELECTOR.FORM.AUTHENTICATE,      ui.loginEvent);
+		$("nav").on( "click",  SELECTOR.BUTTON.LOGOUT,          ui.logoutEvent);
 
 		loginSuccess();
 
 		$(SELECTOR.BUTTON.LOGOUT).hide().attr("disabled", true);
+	};
+
+
+	ui.addSubTaskIssue = function ( issueOrId ) {
+		var issue;
+
+		if (
+			typeof issueOrId.fields != "undefined"
+			&& typeof issueOrId.fields.issuetype != "undefined"
+			&& typeof issueOrId.fields.status != "undefined"
+			&& typeof issueOrId.fields.summary != "undefined"
+		) {
+			issue = issueOrId;
+
+			$('.task_' + issue.fields.issuetype.id +" .list-group").append(
+				$("<li>").addClass("list-group-item existingTask").append(
+					$("<img>")
+							.attr("src", issue.fields.status.iconUrl)
+							.attr("title", issue.fields.status.name)
+				)
+				.append(
+						$("<a>")
+						.attr("href", jrt.issue.parseWebUrl(issue))
+						.attr("target", "_blank")
+						.text(" "+issue.key)
+				).append(" "+issue.fields.summary)
+			);
+
+			$('.task_' + issue.fields.issuetype.id).each(function () {
+				$(this).removeClass("list-group-item-danger").addClass("list-group-item-success");
+				$(this).find(".input-group").removeClass("has-error").addClass("has-success");
+				$(this).find(".badge").text($(this).find(".list-group-item.existingTask").length)
+			});
+		} else {
+			jrt.issue.find( issueOrId, ui.addSubTaskIssue );
+		}
+	};
+
+
+	ui.addSubTaskIssues = function ( issues ) {
+		for ( var i in issues ) {
+			ui.addSubTaskIssue( issues[i] );
+		}
+	};
+
+
+	ui.issueCreateSubTasks = function (){
+		var taskVal, subtask;
+
+		if ($("#issueStatus").html() == '') {
+			jrt.ui.notify( "You must find a valid Jira issue to create sub-tasks" );
+
+			return false;
+		}
+
+		$( "#subTasks input[type=checkbox]" ).each(function () {
+			if($(this).is(':checked')){
+				taskVal = $(this).val();
+
+				subtask = {
+					"parentKey": $("#parentKey").val(),
+					"projectKey": $("#parentKey").val().split("-")[0],
+					"issueTypeId": $("#issueTypeVal_" + taskVal).val(),
+					"issueTypeName": $("#taskrow_" + $("#issueTypeVal_" + taskVal).val() + " img").attr( "title" ),
+					"summary": $("#summary_" + taskVal).val()
+				};
+				jrt.issue.createSubTask( subtask );
+			}
+		});
+
+		// TODO: Change this so that a proper callback is fired after all
+		// subtask creation threads are complete.
+		setTimeout(function(){
+			jrt.issue.find( jrt.ui.getIssueSearchValue(), jrt.ui.issueSearchSuccess );
+		},2000);
 	};
 
 
@@ -172,7 +247,7 @@
 					.text(issue.key)
 			).append(" "+issue.fields.summary);
 
-			getSubTasks( issue.id );
+			jrt.issue.getSubTasks( issue.id );
 		} else {
 			$("#issueSummary").html( $("<span>").addClass("label label-danger").text("ISSUE NOT FOUND") );
 			$("#issueStatus").html("");
@@ -180,6 +255,29 @@
 			resetHighlighting();
 		}
 	}
+
+
+	ui.issuesMarkAll = function () {
+		$("#subTasks input[type=checkbox]").each(function () {
+			$(this).prop("checked", true);
+		});
+	};
+
+
+	ui.issuesMarkRequired = function () {
+		issuesUnmarkAll();
+
+		$(".required input[type=checkbox]").each(function () {
+			$(this).prop("checked", true);
+		});
+	};
+
+
+	ui.issuesUnmarkAll = function (){
+		$("#subTasks input[type=checkbox]").each(function () {
+			$(this).prop("checked", false);
+		});
+	};
 
 
 	ui.loginEvent = function (event) {
@@ -216,9 +314,11 @@
 				&& typeof jqXHR.responseJSON.response.body != "undefined"
 		) {
 			var response = jqXHR.responseJSON.response.body;
-			console.log( response );
+
+			if ( typeof response.errorMessages != "undefined" ) {
+				ui.notify( response.errorMessages.join( "\n" ) );
+			}
 		}
-		//console ? console.log("loginError") : null;
 	}
 
 
@@ -353,16 +453,65 @@
 (function ( iss, undefined ) {
 	var isAuthenticated = false;
 
-	iss.find = function ( keyOrId, success, error ) {
+
+	iss.createSubTask = function ( subtask ){
+		var MIN_SUMMARY_LENGTH = 4;
+
+		if ( subtask.summary.length > MIN_SUMMARY_LENGTH ) {
+			$.ajax({
+				url: jrt.getCreateTaskUrl(),
+				data: subtask,
+				success: function ( data, textStatus, jqXHR ){
+					if ( typeof data.key != "undefined" ) {
+						jrt.ui.addSubTaskIssue( data );
+					}
+				},
+				error: function ( jqXHR, textStatus, errorThrown ) {
+					jrt.ui.notify( "Failed to create subtask\n'"+subtask.summary+"'" );
+				}
+			});
+		} else {
+			ui.notify( "Summary length must be more than "+MIN_SUMMARY_LENGTH+" characters long." );
+		}
+	};
+
+
+	iss.find = function ( issueOrId, success, error ) {
+		var id;
+
+		if ( typeof issueOrId.key != "undefined" ) {
+			id = issueOrId.key;
+		} else {
+			id = issueOrId.toString();
+		}
+
 		$.ajax( getBaseUrl()+"?method=getIssueByKey",{
 			"data":   {
-				"issueKey": keyOrId || ""
+				"issueKey": id || ""
 			},
 			"dataType": "json",
 			"error": error,
 			"success": success
 		});
-	}
+	};
+
+
+	iss.getSubTasks = function ( id ){
+		var data = 'issueid=' + id;
+		$.ajax({
+			url: jrt.getSubtaskUrl(),
+			data: data,
+			success: function( data, textStatus, jqXHR ) {
+				var issues = $.parseJSON( data );
+
+				jrt.ui.removeSubtasks();
+				jrt.ui.addSubTaskIssues( issues );
+			},
+			error: function ( jqXHR, textStatus, errorThrown ) {
+				jrt.ui.notify( "Error finding issue '"+id+"'" );
+			}
+		});
+	};
 
 
 	iss.parseWebUrl = function ( issue ) {
@@ -373,177 +522,6 @@
  * END: Issue module
 **********************************/
 
-
-
-function getSubTasks(id){
-	data = 'issueid=' + id;
-	JQajax(
-		jrt.getSubtaskUrl(),
-		data,
-		handleSubTaskIssues
-	);
-}
-
-
-function handleSubTaskIssues(data){
-	jrt.ui.removeSubtasks();
-
-	var issues = $.parseJSON(data);
-	for ( var i in issues ) {
-		issue = issues[i];
-
-		$('.task_' + issue.fields.issuetype.id +" .list-group").append(
-			$("<li>").addClass("list-group-item existingTask").append(
-				$("<img>")
-						.attr("src", issue.fields.status.iconUrl)
-						.attr("title", issue.fields.status.name)
-			)
-			.append(
-					$("<a>")
-					.attr("href", jrt.issue.parseWebUrl(issue))
-					.attr("target", "_blank")
-					.text(" "+issue.key)
-			).append(" "+issue.fields.summary)
-		);
-
-		$('.task_' + issue.fields.issuetype.id).each(function () {
-			$(this).removeClass("list-group-item-danger").addClass("list-group-item-success");
-			$(this).find(".input-group").removeClass("has-error").addClass("has-success");
-			$(this).find(".badge").text($(this).find(".list-group-item.existingTask").length)
-		});
-	}
-}
-
-
-function createSubTask(){
-	var taskVal, data;
-
-	if ($("#issueStatus").html() == '') {
-		jrt.ui.notify( "You must find a valid Jira issue to create sub-tasks" );
-
-		return false;
-	}
-
-	$("#subTasks input[type=checkbox]").each(function () {
-		if($(this).is(':checked')){
-			taskVal = $(this).val();
-
-			data = {
-				"parentKey": $("#parentKey").val(),
-				"projectKey": $("#parentKey").val().split("-")[0],
-				//"issueTypeId": $("#issueTypeVal_" + taskVal).val(),
-				"issueTypeName": $("#taskrow_" + $("#issueTypeVal_" + taskVal).val() + " img").attr( "title" ),
-				"summary": $("#summary_" + taskVal).val()/* ,
-				"description": "" */
-			};
-
-			if( data.summary.length > 4 ) {
-				JQajax(jrt.getCreateTaskUrl(),data,createSubTaskCallback );
-			}
-		}
-	});
-
-	setTimeout(function(){
-		jrt.issue.find( jrt.ui.getIssueSearchValue(), jrt.ui.issueSearchSuccess );
-	},2000);
-}
-
-
-function createSubTaskCallback(data){
-	try {
-	var obj = jQuery.parseJSON(data);
-} catch (e) {
-	console.log("there was an error parsing create issue response");
-	console.dir(data);
-}
-
-	//$(".issueType_" + obj.TYPEID).prop('checked', false);
-}
-
-
-function buildFormDataJQuery(form,incUnchecked) {
-	var data, my_form;
-
-	try {
-		my_form = $("[name=" + form + "]");
-		data = $(my_form).serialize();
-
-		if (incUnchecked){
-			data += unCheckedBoxes(form);
-			data + emptyMultiSelect(form);
-		}
-	} catch(err) {
-		data = buildFormData(form);
-	}
-
-	return data;
-}
-
-
-function JQajax( url, data, callback ) {
-	var myCaller = arguments.callee.caller;
-
-	$.ajax({
-		data:data,
-		error:function(jqXHR, textStatus, errorThrown){
-			var message = "";
-
-			message += "AJAX Request Failed!";
-			message += "\nStatus: " + textStatus;
-			message += "\nError: " + errorThrown.toString();
-			message += "\nURL: " + url.toString();
-			message += "\nData: " + JSON.stringify( data ).toString();
-			if ( myCaller )
-				message += "\nCalling function: " + myCaller.toString();
-
-			console ? console.log( message ) : null;
-		},
-		success:function(data, textStatus, jqXHR){
-			if (typeof(callback) === 'function') {
-				callback(data);
-			}
-		},
-		url:url.toString()
-	});
-}
-
-
-function checkall() {
-	$("#subTasks input[type=checkbox]").each(function () {
-		$(this).prop("checked", true);
-	});
-}
-
-
-function clearChecked(){
-	$("#subTasks input[type=checkbox]").each(function () {
-		$(this).prop("checked", false);
-	});
-}
-
-
-function checkreq() {
-	clearChecked();
-
-	$(".required input[type=checkbox]").each(function () {
-		$(this).prop("checked", true);
-	});
-}
-
-
-function getIssueTypeIcons () {
-	var type;
-
-	$( ".taskrow" ).each(function(){
-		type = $(this).attr("x-jira-issue-type");
-
-		$(this).find(".task-type").prepend(
-			$("<img>")
-				.attr("src","nowhere")
-				.attr("title","something")
-		);
-	});
-}
 
 
 $(document).ready(jrt.init);
